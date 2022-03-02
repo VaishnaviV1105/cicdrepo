0cbc6a054643faaf5c5abae7257c264a848a1bfd
VaishnaviV1105 <90887354+VaishnaviV1105@users.noreply.github.com>
2022-03-02 11:20:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
 provider "aws" {
     region="us-east-2"
 }
+terraform { 
+
+  required_providers { 
+
+    aws = { 
+
+      source  = "hashicorp/aws" 
+
+      version = "~> 3.0" 
+
+    } 
+
+  } 
+
+} 
 resource "aws_instance" "web" {
 
   ami = "ami-0b614a5d911900a9b"
